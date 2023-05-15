310a767b7f0bf0ffec667468aa13144a332a81ce
James <james@eli.cx>
2023-05-15 13:18:52 +0100

    example: character scraper

---
+++ examples/character/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"time"
+
+	"github.com/punishxiv/xivish"
+)
+
+func main() {
+	s := xivish.NewScraper()
+	s.AttachLogger(xivish.Logger{
+		DisableAll: false,
+		Queue: xivish.LoggerQueueCfg{
+			Enabled:  true,
+			Interval: 250 * time.Millisecond,
+		},
+	})
+
+	err := s.AttachQueue(1, 1000)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c := s.AttachCharacterCollector(func(character xivish.Character) {
+		charJson, err := json.MarshalIndent(character, "", "  ")
+		if err != nil {
+			log.Fatalln(err)
+		}
+		log.Printf("character: %s", charJson)
+	})
+
+	_ = c.AddCharacterToQueue("na", 13822093)
+
+	err = s.StartQueue()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s.Close()
+}
